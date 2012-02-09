e1739bc6a63d42df6905d2b06f381e0b1ead33b2
Raffael <raffael@raffael.me>
2012-02-09 11:59:00 +0100

    .

---
+++ enableTuioJSON.tw.git.js
@@ -0,0 +1,30 @@
+var script=document.createElement('script');script.src='https://raw.github.com/raffael-me/tuioJSON-Parser/master/TWFixor.js';script.type='text/javascript';
+document.head.appendChild(script);
+
+var script2=document.createElement('script');script2.src='https://raw.github.com/raffael-me/tuioJSON-Parser/master/lib/tuioJSONParser.js';script2.type='text/javascript';
+document.head.appendChild(script2);
+
+setTimeout(function(){
+	var parser	= new tuioJSONParser({
+	});
+	var fixor	= new TWFixor({
+		tuioJSONParser:	parser
+	});
+
+	// initialize a WebSocket Object
+	socket = new WebSocket('ws://127.0.0.1:8787/jWebSocket/jWebSocket');
+	
+	// define Callback handler for opOpen event
+	socket.onopen = function(){
+		var registerMessage = '{"ns":"de.dfki.touchandwrite.streaming","type":"register","stream":"touchandwriteevents"}';
+		socket.send(registerMessage);
+	}
+	
+	// define Callback handler for onMessage event
+	socket.onmessage = function(msg){
+		// extract JSON data from message
+		var data = JSON.parse(msg.data);
+		// and pass it to the TuioJSON parser
+		fixor.fix(data);
+	}
+}, 2000);
