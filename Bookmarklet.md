a992af11a73b451b126c06410eb902376800aed6
Raffael <raffael@raffael.me>
2012-02-12 14:42:40 +0100

    Enhanced enableTuoJSON loader, customizable names for the Touch and Pen events, JavaScript bookmarklet.

---
+++ Bookmarklet.md
@@ -0,0 +1,6 @@
+# Bookmarklet
+You can use the following code as Bookmarklet to enable Touch Responsiveness using the tuioJSON technology for the current website you are visiting:
+
+	javascript:(function(){window.enabler=document.createElement('script');enabler.src='http://localhost/bachelor/tuioJSON%20Parser/enableTuioJSON.tw.js';enabler.type="text/javascript";document.head.appendChild(enabler);})();
+
+<a href="javascript:(function(){window.enabler=document.createElement('script');enabler.src='http://localhost/bachelor/tuioJSON%20Parser/enableTuioJSON.tw.js';enabler.type="text/javascript";document.head.appendChild(enabler);})();" style="display:block; background:grey; border: 1px solid black; padding: 5px;">Drag Me to your Bookmarks bar!</a>
--- enableTuioJSON.tw.js
+++ enableTuioJSON.tw.js
@@ -1,8 +1,47 @@
 /**
+ * Use the following code for a bookmark:
+ * 
+	javascript:(function(){ var enabler	= document.createElement('script');enabler.src='http://localhost/bachelor/tuioJSON%20Parser/enableTuioJSON.tw.js';enabler.type="text/javascript";document.head.appendChild(enabler);})();
+ *
+ */
+
+/**
  * state variables
  */
 var tuioParserComplete	= false,
 	twFixorComplete		= false;
+
+
+/**
+ * as soon as both scripts have been loaded, create the parser and the WebSocket connection
+ */
+function injectingTuioComplete(){
+	if (tuioParserComplete && twFixorComplete) {
+		window.parserProxy	= new TWFixor({
+			tuioJSONParser:	new tuioJSONParser({
+				// parsing options
+			})
+		});
+		
+		// initialize a WebSocket Object
+		socket = new WebSocket('ws://127.0.0.1:8787/jWebSocket/jWebSocket');
+		
+		// define Callback handler for opOpen event
+		socket.onopen = function(){
+			var registerMessage = '{"ns":"de.dfki.touchandwrite.streaming","type":"register","stream":"touchandwriteevents"}';
+			socket.send(registerMessage);
+			console.log("tuioJSON is ready");
+		}
+		
+		// define Callback handler for onMessage event
+		socket.onmessage = function(msg){
+			// extract JSON data from message
+			var data = JSON.parse(msg.data);
+			// and pass it to the TuioJSON parser
+			window.parserProxy.fix(data);
+		}
+	}
+}
 
 /**
  * prepare script injecting and define onload event handlers
@@ -30,33 +69,3 @@ scriptTWFixor.onload	= function(){
  */
 document.head.appendChild(scriptTuioParser);
 document.head.appendChild(scriptTWFixor);
-
-/**
- * as soon as both scripts have been loaded, create the parser and the WebSocket connection
- */
-function injectingTuioComplete(){
-	if (tuioParserComplete && twFixorComplete) {
-		window.parserProxy	= new TWFixor({
-			tuioJSONParser:	new tuioJSONParser({
-				// parsing options
-			})
-		});
-		
-		// initialize a WebSocket Object
-		socket = new WebSocket('ws://127.0.0.1:8787/jWebSocket/jWebSocket');
-		
-		// define Callback handler for opOpen event
-		socket.onopen = function(){
-			var registerMessage = '{"ns":"de.dfki.touchandwrite.streaming","type":"register","stream":"touchandwriteevents"}';
-			socket.send(registerMessage);
-		}
-		
-		// define Callback handler for onMessage event
-		socket.onmessage = function(msg){
-			// extract JSON data from message
-			var data = JSON.parse(msg.data);
-			// and pass it to the TuioJSON parser
-			window.parserProxy.fix(data);
-		}
-	}
-}
